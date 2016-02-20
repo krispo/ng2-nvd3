685159f8d08f4f61e0cbee6e1cdae637db2deb74
krispo <konstantin.skipor@gmail.com>
2016-02-20 19:26:42 +0300

    added some tests

--- test/ng2-nvd3.spec.ts
+++ test/ng2-nvd3.spec.ts
@@ -5,7 +5,10 @@ import {nvD3} from '../lib/ng2-nvd3';
 declare let describe, beforeEach, it, expect, d3: any;
 declare let currentChartType: string;
 const chartTypes = [
-  'discreteBarChart'
+  'lineChart',
+  'discreteBarChart',
+  'pieChart',
+  'scatterChart'
 ];
 
 //
@@ -55,9 +58,9 @@ const runTests = () => {
       expect(document.querySelectorAll('.main').length).toEqual(1);
     })
 
-    chartTypes.forEach((t) => {
-      it(chartTypes[0] + ' chart type should be created correctly', (done) => {
-        currentChartType = chartTypes[0];
+    chartTypes.forEach((type) => {
+      it(type + ' chart type should be created correctly', (done) => {
+        currentChartType = type;
         bootstrap(Main)
           .then((main) => {
             let options = main.instance.options;
@@ -86,72 +89,58 @@ runTests();
 // Options and Data definitions
 //
 const allOptions = {
+  lineChart:{
+    chart: {
+      type: 'lineChart',
+      height: 450,
+      x: function(d){ return d.x; },
+      y: function(d){ return d.y; }
+    }
+  },
   discreteBarChart: {
     chart: {
       type: 'discreteBarChart',
       height: 450,
-      margin : {
-        top: 20,
-        right: 20,
-        bottom: 50,
-        left: 55
-      },
       x: function(d){return d.label;},
-      y: function(d){return d.value;},
-      showValues: true,
-      valueFormat: function(d){
-        return d3.format(',.4f')(d);
-      },
-      duration: 500,
-      xAxis: {
-        axisLabel: 'X Axis'
-      },
-      yAxis: {
-        axisLabel: 'Y Axis',
-        axisLabelDistance: -10
-      }
+      y: function(d){return d.value;}
+    }
+  },
+  pieChart: {
+    chart: {
+      type: 'pieChart',
+      height: 500,
+      x: function(d){return d.key;},
+      y: function(d){return d.y;}
+    }
+  },
+  scatterChart: {
+    chart: {
+      type: 'scatterChart',
+      height: 450
     }
   }
 }
 
 const allData = {
+  lineChart: [
+    { key: 'key1', values: [{x: 0, y: 0},{x: 1, y: 1}] },
+    { key: 'key2', values: [{x: 1, y: 1},{x: 0, y: 0}] }
+  ],
   discreteBarChart: [
     {
       key: "Cumulative Return",
       values: [
-        {
-          "label" : "A" ,
-          "value" : -29.765957771107
-        } ,
-        {
-          "label" : "B" ,
-          "value" : 0
-        } ,
-        {
-          "label" : "C" ,
-          "value" : 32.807804682612
-        } ,
-        {
-          "label" : "D" ,
-          "value" : 196.45946739256
-        } ,
-        {
-          "label" : "E" ,
-          "value" : 0.19434030906893
-        } ,
-        {
-          "label" : "F" ,
-          "value" : -98.079782601442
-        } ,
-        {
-          "label" : "G" ,
-          "value" : -13.925743130903
-        } ,
-        {
-          "label" : "H" ,
-          "value" : -5.1387322875705
-        }
+        { "label" : "A" , "value" : 10 },
+        { "label" : "B" , "value" : 20 }
       ]
     }
-  ]
+  ],
+  pieChart: [
+    { key: "One", y: 5 },
+    { key: "Two", y: 2 }
+  ],
+  scatterChart: [
+    { key: 'key1', values: [{x: 0, y: 0},{x: 1, y: 1}] },
+    { key: 'key2', values: [{x: 1, y: 1},{x: 0, y: 0}] }
+  ],
 }
