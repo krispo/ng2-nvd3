8ef801efc230c8b580ecc41da4e15f6c4771f608
krispo <konstantin.skipor@gmail.com>
2016-05-02 21:21:12 +0300

    add force directed graph

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,7 @@
-import {bootstrap}  from 'angular2/platform/browser';
-import {Component} from 'angular2/core';
-import {nvD3} from '//cdn.rawgit.com/krispo/ng2-nvd3/master/lib/ng2-nvd3.ts';
-import {ChartTypes, AllOptions, AllData} from './defs';
+import {bootstrap}  from 'angular2/platform/browser'
+import {Component} from 'angular2/core'
+import {nvD3} from '//cdn.rawgit.com/krispo/ng2-nvd3/master/lib/ng2-nvd3.ts'
+import {ChartTypes, AllOptions, AllData} from './defs'
 import {ChartSelector} from './chart-selector'
 
 @Component({
