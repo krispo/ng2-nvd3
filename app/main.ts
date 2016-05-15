534695cc806fd8d0fe46f3eccb4f4812d6aa2ec4
krispo <konstantin.skipor@gmail.com>
2016-05-15 14:49:39 +0300

    enable prod mode

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import {bootstrap}  from '@angular/platform-browser-dynamic'
 import {Component} from '@angular/core'
+import {enableProdMode} from '@angular/core'
 import {nvD3} from '//cdn.rawgit.com/krispo/ng2-nvd3/v1.1.0/lib/ng2-nvd3.ts'
 import {ChartTypes, AllOptions, AllData} from './defs'
 import {ChartSelector} from './chart-selector'
@@ -26,4 +27,5 @@ export class Main {
   }
 }
 
+enableProdMode();
 bootstrap(Main);
