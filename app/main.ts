0a7622aca0545903940b8a596f79f7bf25e2f229
krispo <konstantin.skipor@gmail.com>
2016-05-15 14:39:58 +0300

    fix version link

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 import {bootstrap}  from '@angular/platform-browser-dynamic'
 import {Component} from '@angular/core'
-import {nvD3} from '//cdn.rawgit.com/krispo/ng2-nvd3/master/lib/ng2-nvd3.ts'
+import {nvD3} from '//cdn.rawgit.com/krispo/ng2-nvd3/v1.1.0/lib/ng2-nvd3.ts'
 import {ChartTypes, AllOptions, AllData} from './defs'
 import {ChartSelector} from './chart-selector'
 
