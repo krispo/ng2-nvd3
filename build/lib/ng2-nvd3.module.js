c4fc778e047ca57de9398f4af5c0fddabc542636
JR McEntee <john.mcentee@semanticbits.com>
2016-12-29 11:04:29 -0500

    Adding metadata.json files

---
+++ build/lib/ng2-nvd3.module.js
@@ -0,0 +1,16 @@
+"use strict";
+var core_1 = require('@angular/core');
+var ng2_nvd3_1 = require('./ng2-nvd3');
+var nvD3 = (function () {
+    function nvD3() {
+    }
+    nvD3.decorators = [
+        { type: core_1.NgModule, args: [{
+                    declarations: [ng2_nvd3_1.nvD3Component],
+                    exports: [ng2_nvd3_1.nvD3Component]
+                },] },
+    ];
+    nvD3.ctorParameters = function () { return []; };
+    return nvD3;
+}());
+exports.nvD3 = nvD3;
